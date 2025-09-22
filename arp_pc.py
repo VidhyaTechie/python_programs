9cd2ff4cd7f00e7557321ddf5224e41272e3156a
VidhyaTechie <vidhyaselvam456@gmail.com>
2025-09-22 11:31:51 +0530

    Update arp_pc.py

--- arp_pc.py
+++ arp_pc.py
@@ -2,7 +2,7 @@ arp_a="""Internet Address      Physical Address      Type
  10.1.1.1              0030.f29b.0c01        dynamic
  10.1.1.2              000d.bd5d.2eb0        dynamic
 """
-pattern=r"(\d+.*)"
-match=re.findall(pattern,arp_a)
-for item in match:
-    print(item)
+
+output = re.findall(r'(\d+\.\d+\.\d+\.\d+)\s+([0-9a-fA-F\.]+)\s+(\w+)', arp_a)
+for ip,mac,dynamic in output:
+     print(f"IP: {ip}, MAC: {mac}, Type: {dynamic}")
